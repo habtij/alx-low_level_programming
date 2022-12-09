97644049d397cce4454876462b4a49e6f94386e4
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-09 04:28:25 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower;
+	char upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
+	}
+	putchar('\n');
+	return (0);
+}
