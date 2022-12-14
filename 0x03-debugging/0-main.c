a1ecd0022e253d219b1b7e2947b79a42a9aa7fa1
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-14 08:58:49 -0800

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	positive_or_negative(i);
+
+	return (0);
+}
