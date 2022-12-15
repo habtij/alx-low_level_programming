27795b4361f73f8ac4c349fada03b69b178d052e
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-15 02:43:58 -0800

    task 2 updated x8

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,23 +12,23 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a >= b)
 	{
-		if (a > c)
+		if (a >= c)
 			largest = a;
 		else
 			largest = c;
 	}
-	else if (b > a)
+	else if (b >= a)
 	{
-		if (b > c)
+		if (b >= c)
 			largest = b;
 		else
 			largest = c;
 	}
-	else if (c > a)
+	else if (c >= a)
 	{
-		if (c > b)
+		if (c >= b)
 			largest = c;
 		else
 			largest = b;
