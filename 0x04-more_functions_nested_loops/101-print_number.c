e5ffe867d9460cb95261dfbc9b8c93be63328653
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-15 11:56:27 -0800

    task 101 commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - Print integer argument
+ * @n: argument
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
