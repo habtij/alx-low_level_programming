141c0e7f20d5d579307818bcd0b830ab63bfdc84
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-15 06:48:42 -0800

    task 0 commit

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int _add(int, int);
+int _isupper(int);
+
+#endif /* MAIN_H */
