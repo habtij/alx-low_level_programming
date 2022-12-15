c37e5c5d3b279ade4b2a6c03bc11f0173ca35172
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-15 10:49:28 -0800

    task 9 commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints fizz or buzz for multiples of 3, 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ",n);
+		}
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
