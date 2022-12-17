d399c036d34dcb36df50bf24308df9d5786445f0
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-16 16:50:43 -0800

    task 3 updated x3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -26,17 +26,18 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-			/* printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year); */
+			/*
+			 * printf("Day of the year: %d\n", day);
+			 * printf("Remaining days: %d\n", 366 - day);
+			 */
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-			/*
-			 * printf("Day of the year: %d\n", day);
-			 * printf("Remaining days: %d\n", 365 - day);
-			 */
+			/* printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year); */
+
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
