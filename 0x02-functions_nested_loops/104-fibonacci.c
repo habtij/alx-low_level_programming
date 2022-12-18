eb8d7151a4cc4c941f005b41ee32d6c99eac1fee
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-18 14:41:49 -0800

    task 104 commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints fibonacci series
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int latest_val = 2, last_val = 1, i = 0;
+
+	printf("1, 2, ");
+	while (i < 98)
+	{
+		latest_val = latest_val + last_val;
+		printf("%d", latest_val);
+		last_val = latest_val - last_val;
+		i++;
+		if (i != 98)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
