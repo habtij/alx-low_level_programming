4b735c3e0c02963cf8259542f28ba14879b33af4
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-20 09:03:18 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - print the second half of the string
+ * @str: pointer argument
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, j, half, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	if ((len - 1) % 2 != 0)
+	{
+		_putchar(str[len - 1]);
+	}
+	else
+	{
+		half = (len - 1) / 2;
+		for (j = half; str[j] != '\0'; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
