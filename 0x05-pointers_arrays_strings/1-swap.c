75ce641524fdfecfa8aa220100eb77147ab897f3
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-19 20:21:41 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of arguments
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int *first = &a;
+	int second = &b;
+
+	*first = b;
+	*second = a;
+}
