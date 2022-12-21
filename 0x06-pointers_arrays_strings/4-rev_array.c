244a145db3fc4bafc098c948f1e513a26942c2df
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-21 15:06:24 -0800

    task 4 updated x5

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * print_number - helper function
- * @n: first argument
- *
- * Return: void
- */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
-}
-
-/**
  * reverse_array - reverses the content of an array of integers
  * @a: first argument
  * @n: second argument
@@ -30,16 +9,10 @@ void print_number(int n)
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, j;
 
-	for (i = n - 1; i >= 0; i--)
+	for (i = n - 1, j = 0; i >= 0; i--, j++)
 	{
-		print_number(a[i]);
-		if (i != 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		a[j] = a[i];
 	}
-	_putchar('\n');
 }
