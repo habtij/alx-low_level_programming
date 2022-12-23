16da834946a146a714f68d3d8bc8deeb94a39fd4
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-23 10:33:20 -0800

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,4 +6,23 @@
  *
  * Return: char
  */
-char string_toupper(char *s)
+char *string_toupper(char *s)
+{
+	int i, j;
+	char upper[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+			'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			'W', 'X', 'Y', 'Z'};
+	char lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+			'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+			'w', 'x', 'y', 'z'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 26; j++)
+		{
+			if (lower[j] == s[i])
+				*(s + i) = upper[j];
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *leet(char *);
+char *string_toupper(char *);
 
 #endif /* MAIN_H */
