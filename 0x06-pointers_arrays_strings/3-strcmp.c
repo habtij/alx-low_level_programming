e4af74a63c7804bb9f11d0ba2167b7e3fe337de5
Tijani Habeeb <tijanihabeeb12@outlook.com>
2022-12-23 00:31:29 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,29 +9,32 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, s1_len = 0, s2_len = 0;
-	int diff;
+	int i, j, k, s1_len = 0, s2_len = 0;
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		s1_len++;
-	}
 
 	for (j = 0; s2[j] != '\0'; j++)
-	{
 		s2_len++;
-	}
 
-	if (s1_len == s2_len)
+	if (s1_len != s2_len)
 	{
-		for (i = 0; i < s1_len; i++)
+		if (s1_len > s2_len)
 		{
-			if (s1[i] != s2[i])
-			{
-				diff = s1[s1_len] - s2[s2_len];
-				break;
-			}
+			return ((int)(unsigned char)s1[s1_len - 1]);
+		}
+		else
+		{
+			return (-(int)(unsigned char)s2[s2_len - 1]);
 		}
 	}
-	return (diff);
+	else
+	{
+		for (k = 0; k <= s1_len; k++)
+		{
+			if (s1[k] != s2[k])
+				return ((int)(unsigned char)s1[s1_len - 1] - (int)(unsigned char)s2[s1_len - 1]);
+		}
+	}
+	return (0);
 }
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @s: char argument
+ *
+ * Return: char
+ */
+char string_toupper(char *s)
