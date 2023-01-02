693644ee960017a5a8039699734e3c8ad1a793d6
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-02 01:39:57 -0800

    task 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
