4390e214055f9d04cc146ace1626b58a65f41271
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-03 13:30:49 -0800

    task 3 updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,15 +12,17 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int n = 0;
 	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (accept[j] == s[i])
 			{
 				n++;
 				break;
 			}
+			else if (accept[j + 1] == '\0')
+				return (n);
 		}
 	}
 	return (n);
