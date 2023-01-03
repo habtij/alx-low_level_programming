92ade8ee6d08e7d6297f0446306cff92bd64c2cc
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-03 13:54:06 -0800

    task 8 updated

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,11 +13,13 @@ void print_diagsums(int *a, int size)
 {
 	int right_sum = 0, left_sum = 0, i, j;
 
-	j = size - 1;
 	for (i = 0; i < size; i++)
 	{
-		left_sum += a[i];
-		j--;
+		left_sum += a[i * size + i];
+	}
+	for (j = size - 1; j >= 0; j--)
+	{
+		right_sum += a[j * size + (size - j - 1)];
 	}
 	printf("%d, %d\n", left_sum, right_sum);
 }
