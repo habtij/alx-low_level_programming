bcfbae91d6909bed3dad853283120c31a8e46203
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-03 14:03:02 -0800

    task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of pointerto character
+ * @s: pointer argument
+ * @to: character argument
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
