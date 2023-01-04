fa54e0fb9dfb5673311236905df6153aa6733c6a
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-04 14:56:11 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of string
+ * @s: pointer argument
+ *
+ * Return: integer
+ */
+int len = 0;
+int _strlen_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		len += 1;
+		_strlen_recursion(s + 1);
+	}
+	return (len);
+}
