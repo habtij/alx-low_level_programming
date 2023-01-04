0fbc2dc7a4041c901c8ccdd11251a769c4518f10
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-04 14:23:07 -0800

    task 1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -22,5 +22,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif /* MAIN_H */
