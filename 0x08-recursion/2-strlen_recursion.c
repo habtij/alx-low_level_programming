61bb0df358ba034c61f82df5f402e4f9a7592f74
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-05 14:26:07 -0800

    task 2 updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,10 @@
  *
  * Return: integer
  */
-int len = 0;
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		len += 1;
-		_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return (len);
 }
