f0869201b0c3d05d670c854321f0bda324d04c1e
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-06 18:43:31 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: counter argument
+ * @argv: vector argument
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
