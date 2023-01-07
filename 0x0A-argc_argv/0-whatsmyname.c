06828d191379c80634cbf38e8d6515efd8ee20ba
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-06 18:26:47 -0800

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print my file name
+ * @argc: counter argument
+ * @argv: vector argument
+ *
+ * Return: Always (0);
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
