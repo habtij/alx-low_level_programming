f82220994419891d8017d8c6107a58d7cbc1895e
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-06 19:29:58 -0800

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add the arguments and output to stdout
+ * @argc: counter argument
+ * @argv: vector argument
+ *
+ * Return: 0 on success,
+ *	1, on failure
+ */
+int main(int argc, char **argv)
+{
+	int i, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+}
