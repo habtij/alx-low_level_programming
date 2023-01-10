0b1c81d089aa8d865ffdbb8e7a14ab1cada7c511
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-10 01:21:17 -0800

    task 3 updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,11 +31,14 @@ int **alloc_grid(int width, int height)
 	{
 		for (j = 0; j < width; j++)
 		{
-			p[i][j] = num;
+			if (p[i] != NULL)
+			{
+				p[i][j] = num;
+			}
 		}
 	}
-	return (p);
 
+	return (p);
 	for (i = 0; i < height; i++)
 	{
 		free(p[i]);
