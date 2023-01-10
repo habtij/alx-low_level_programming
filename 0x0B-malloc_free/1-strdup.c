991f3de7eeb56bbcdd117d8d7eb5de664b8e27d6
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-09 23:19:13 -0800

    task 2 updated x4

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 	char *p;
 
 	len = strlen(str);
-	if (str != '\0')
+	if (*str != '\0')
 		return (NULL);
 
 	if (len == 1)
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
+	if (*s1 != '\0' || *s2 != '\0')
 	{
 		return (NULL);
 	}
