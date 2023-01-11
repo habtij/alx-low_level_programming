650943a18b49602f15457029bd0adbe0bc1affbb
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-11 13:40:15 -0800

    task 101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * strtow - a function that splits a string into words
+ * @str: pointer to a string
+ *
+ * Return: pointer to an array
+ */
+char **strtow(char *str)
+{
+	int i;
+	char **p;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+
+}
