600dc59cf717cb9b92700bd380767ab7a5647412
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-14 02:10:42 -0800

    task 100 update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,8 +36,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 		if (ptr == NULL)
 		{
-			return (str);
 			free(str);
+			return (str);
 		}
 
 		/* memset(str, ptr, new_size); */
