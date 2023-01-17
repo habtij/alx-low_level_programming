0f73670b1875d51a857286554835117a565c0d71
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-17 12:02:32 -0800

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -12,6 +12,16 @@ void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+			d->name = NULL;
+		}
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+			d->owner = NULL;
+		}
 		free(d);
 	}
 }
