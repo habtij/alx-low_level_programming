d6e7360095ef734d8b72559576efaed1e1567b80
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-18 05:04:41 -0800

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - a function that prints a name
@@ -9,5 +10,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(*name);
+	if (name != NULL)
+		f(name);
 }
