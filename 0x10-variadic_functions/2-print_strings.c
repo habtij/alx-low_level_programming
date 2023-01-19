c0f5c6046ef3593e3300824c7c907050c4dbe6ca
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-19 09:57:52 -0800

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_strings - a function that prints strings, followed by a new line.
@@ -15,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *s;
 
 	va_start(str, n);
-	for (i = 0; i < n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		s = va_arg(str, char *);
 		if (s == NULL)
