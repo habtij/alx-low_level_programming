a45f41e93c8222617fa37cdad2a3ca81c888c547
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-19 23:59:44 -0800

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,7 +11,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[6] = {
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -24,13 +24,12 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (s == ops[i].op)
+		if (*s == *(ops[i].op))
 		{
 			return (ops[i].f);
 		}
 		i++;
 	}
-	printf("%s\n", ops[i].op);
 	printf("Error\n");
 	exit(99);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,13 +11,25 @@
  */
 int main(int argc, char **argv)
 {
+	char *op;
+	int n1, n2;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	printf("%d\n", (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+	n1 = atoi(argv[1]);
+	op = (char *)argv[2];
+	n2 = atoi(argv[3]);
+	if ((*op == '/' && n2 == 0) ||
+			(*op == '%' && n2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(n1, n2));
 
 	return (0);
 }
