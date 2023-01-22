3a02168efdb43a63ee8c702850e9d8bb2cc23b0a
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-21 22:28:59 -0800

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,6 +4,44 @@
 #include <string.h>
 
 /**
+ * check_format - helper function for checking type of format
+ * @a: a va_list type argument
+ * @c: format to check
+ *
+ * Return: nothing
+ */
+void check_format(va_list a, char c)
+{
+	int int_n;
+	double f_n;
+	char c_val;
+	char *s_val;
+
+	switch (c)
+	{
+	case 'c':
+		c_val = va_arg(a, int);
+		printf("%c", c_val);
+		break;
+	case 'i':
+		int_n = va_arg(a, int);
+		printf("%d", int_n);
+		break;
+	case 'f':
+		f_n = va_arg(a, double);
+		printf("%f", f_n);
+		break;
+	case 's':
+		s_val = va_arg(a, char *);
+		if (s_val == NULL)
+			s_val = "(nil)";
+
+		printf("%s", s_val);
+		break;
+	}
+}
+
+/**
  * print_all - a function that prints anything.
  * @format: variadic argument
  *
@@ -12,39 +50,14 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i, int_n, len;
-	double f_n;
-	char c_val;
-	char *s_val;
+	int i, len;
 
 	len = strlen(format);
 	i = 0;
 	va_start(args, format);
 	while (i < len)
 	{
-		switch (format[i])
-		{
-		case 'c':
-			c_val = va_arg(args, int);
-			printf("%c", c_val);
-			break;
-		case 'i':
-			int_n = va_arg(args, int);
-			printf("%d", int_n);
-			break;
-		case 'f':
-			f_n = va_arg(args, double);
-			printf("%f", f_n);
-			break;
-		case 's':
-			s_val = va_arg(args, char *);
-			if (s_val == NULL)
-			{
-				s_val = "(nil)";
-			}
-			printf("%s", s_val);
-			break;
-		}
+		check_format(args, format[i]);
 		if (i != (len - 1) && (format[i] == 'c' || format[i] == 'i'
 				|| format[i] == 'f' || format[i] == 's'))
 			printf(", ");
