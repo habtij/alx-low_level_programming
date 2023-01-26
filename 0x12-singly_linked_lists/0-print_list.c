0d23e9e7278700c2388db9ec8078ad39ea1e9570
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-26 11:10:10 -0800

    task 0 & 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
---
+++ 0x12-singly_linked_lists/list_len.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * list_len - count number of nodes
+ * @h: head node pointer
+ *
+ * Return: number of nodes
+ */
+size_t 
