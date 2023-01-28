ce5c18b10de7a39ba0905f7f19fabad4d3ee18fc
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-27 16:06:14 -0800

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - prints before main function
+ *
+ * Return: nothing
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
