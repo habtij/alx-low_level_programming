70e774f31e8733a8deebdc2018bd1d39d2310fda
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-27 17:08:07 -0800

    task 101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,10 @@
+global    main
+          extern    printf
+
+          section   .text
+main:  
+          mov       rdi, message
+          call      printf
+          ret
+message:
+          db        "Hello, Holberton", 0
