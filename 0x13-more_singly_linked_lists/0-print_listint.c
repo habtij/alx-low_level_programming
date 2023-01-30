aadab68e9ad534d9de6541e5546f54ea1d2e15a3
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-30 09:41:14 -0800

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_listint - prints content of a linked list
+ * @h: pointer to head node
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->n != NULL)
+		{
+			printf("%d\n", h->n);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
