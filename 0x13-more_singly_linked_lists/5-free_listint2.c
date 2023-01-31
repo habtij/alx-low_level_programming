602e0d3f12554e2ca8c7f10fc67c24ee7ead0adc
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-01-31 12:39:42 -0800

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,8 +14,8 @@ void free_listint2(listint_t **head)
 	while (*head != NULL)
 	{
 		temp = *head;
-		*head = *head->next;
-		free(*head);
+		*head = (*head)->next;
+		free(temp);
 	}
 	*head = NULL;
 }
