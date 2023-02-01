b0057ae66b617dae9a2219d57ca391b60209db2a
Tijani Habeeb <tijanihabeeb12@outlook.com>
2023-02-01 14:11:31 -0800

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -17,5 +17,7 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
-	*head = NULL;
+
+	if (*head != NULL)
+		*head = NULL;
 }
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint - inserts new node to idx
+ * @head: vector to pointer head
+ * @idx: index value of new node
+ * @n: value of new node
+ *
+ * Return: 
