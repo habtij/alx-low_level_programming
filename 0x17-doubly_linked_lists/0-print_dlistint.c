8ebf2e21d5f191c0703b83b07f3010d89e4059a1
Habeeb <tijanihabeeb12@outlook.com>
2023-04-01 12:54:09 +0100

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,13 +10,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t count;
+size_t count;
 
-  while (h != NULL)
-  {
-    printf("%d\n", h->n);
-    h = h->next;
-    count++;
-  }
-  return (count);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count += 1;
+}
+return (count);
 }
