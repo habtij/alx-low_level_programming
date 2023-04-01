fc10eff6e00f6fd997ae3c3876524e73fd1d3137
Habeeb <tijanihabeeb12@outlook.com>
2023-04-01 16:36:19 +0100

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,3 +9,22 @@
  * Return: address of the new element, or NULL if failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new;
+
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+{
+  return (NULL);
+  free(new);
+}
+
+new->n = n;
+*head->prev = new;
+new->next = *head;
+*new->prev = NULL;
+*head = new;
+
+return (new);
+free(new);
+}
