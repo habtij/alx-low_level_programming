4cbb77ad2ab6a0737f09274bd2f6fc099de8c2ce
Habeeb <tijanihabeeb12@outlook.com>
2023-09-14 20:16:00 +0100

    Add: 0. Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_dlistint - prints all the elements of DLL list
+ * @h: pointer to the head of the DLL
+ *
+ * Return: size of the linked list, or 0
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t n = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+n++;
+}
+
+return (n);
+}
