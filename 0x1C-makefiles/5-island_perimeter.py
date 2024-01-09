7a326abe89fafe008af0e6ec26fead6a42ca9180
Habeeb <tijanihabeeb12@outlook.com>
2024-01-09 11:06:13 +0100

    Update: 5. island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,7 +14,7 @@ def island_perimeter(grid):
         for j in range(width):
             if grid[i][j] == 1:
                 size += 1
-                if (j > 0 and grid[i]pj - 1] == 1):
+                if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
